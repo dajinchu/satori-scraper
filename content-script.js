26efe971376320d7e31bc9b5aa5bcbcd1386a3c3
lenny-z <zhoulangchun@gmail.com>
2017-06-17 15:54:18 -0400

    Boi

---
+++ content-script.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	var scraperUtilityDiv = document.createElement("div");
+
+	//insert the scraper utility div before the first body element
+	document.body.insertBefore(scraperUtilityDiv, document.body.firstChild);
+
+	scraperUtilityDiv.style.position = "fixed";
+	
+	//load scraper.html into scraperUtilityDiv
+	$(scraperUtilityDiv).load(chrome.extension.getURL("html/scraper.html"));
+	/* scraperUtilityDiv.innerHTML = "<object type='text/html' data='scraper.html'></object>"; */
+}
