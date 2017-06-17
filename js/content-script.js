cdc83bef4ad3e6466262688d3013a778ddea7791
lenny-z <zhoulangchun@gmail.com>
2017-06-17 19:48:29 -0400

    Inject everything

---
+++ js/content-script.js
@@ -0,0 +1,20 @@
+//var scraperUtilityCSS = document.createElement("style");
+
+//scraperUtilityCSS.src = chrome.extension.getURL("css/styles.css");
+//scraperUtilityScript.src = chrome.extension.getURL("js/scraper-script.js")
+
+console.log(document.body.firstChild.id);
+
+var CSSNode = document.createElement("link");
+CSSNode.rel = "stylesheet";
+CSSNode.href = chrome.extension.getURL("css/styles.css");
+document.head.appendChild(CSSNode);
+
+var scriptNode = document.createElement("script");
+scriptNode.src = chrome.extension.getURL("js/scraper-script.js");
+document.head.appendChild(scriptNode);
+
+var scraperTool = document.createElement("header");
+scraperTool.id = "scraper-tool";
+document.body.insertBefore(scraperTool, document.body.firstChild);
+$(scraperTool).load(chrome.extension.getURL("html/scraper.html"));
