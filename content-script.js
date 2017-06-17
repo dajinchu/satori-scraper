4a9549bda3a472ef276777754f6a21be53eaaa86
lenny-z <zhoulangchun@gmail.com>
2017-06-17 17:16:23 -0400

    Added more HTML

--- content-script.js
+++ content-script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	var scraperUtilityDiv = document.createElement("div");
+	var scraperUtilityDiv = document.createElement("header");
 
 	//insert the scraper utility div before the first body element
 	document.body.insertBefore(scraperUtilityDiv, document.body.firstChild);
@@ -9,5 +9,6 @@ window.onload = function(){
 	
 	//load scraper.html into scraperUtilityDiv
 	$(scraperUtilityDiv).load(chrome.extension.getURL("html/scraper.html"));
+	
 	/* scraperUtilityDiv.innerHTML = "<object type='text/html' data='scraper.html'></object>"; */
 }
